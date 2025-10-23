a1a314981199fbcbe67f3d1545f2e7dfbc0bf759
김혜인 <rjqnrdl7524@naver.com>
2025-10-23 10:02:40 +0900

    LivingEntity, IDamagable 작성

---
+++ Assets/Scripts/LivingEntity.cs
@@ -0,0 +1,35 @@
+using System;
+using UnityEngine;
+
+public class LivingEntity : MonoBehaviour, IDamagable
+{
+    public float MaxHealth = 100f;
+
+    public float Health { get; protected set; }
+    public bool IsDead { get; private set; }
+
+    public event Action OnDeath;
+
+
+    protected virtual void OnEnable()
+    {
+        IsDead = false;
+        Health = MaxHealth;
+    }
+
+    protected virtual void Die()
+    {
+        IsDead = true;
+        OnDeath?.Invoke();
+    }
+
+    public virtual void OnDamage(int damage)
+    {
+        Health -= damage;
+
+        if (Health <= 0 && !IsDead)
+        {
+            Die();
+        }
+    }
+}
